fd18042d9c8ea09aba5b7d0276f356ba728c41db
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-09-20 16:20:50 -0300

    update graficos con promedios

--- src/views/AuditSummary/AuditSummary.tsx
+++ src/views/AuditSummary/AuditSummary.tsx
@@ -1,33 +1,65 @@
-import { useNavigate } from 'react-router-dom'
-import './AuditSummary.css'
-import { AverageModules, BPMGraph, Summary } from '../../components'
-
+import { useNavigate } from 'react-router-dom';
+import './AuditSummary.css';
+import {AverageModules , BPMGraph, Summary} from '../../components/index';
+import { useContext } from 'react';
+import { AppContext } from '../../context/GlobalState';
 
 const AuditSummary: React.FC = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const context = useContext(AppContext);
 
-  const moduleData = [
-    { moduleName: 'Módulo 1', percentage: 85 },
-    { moduleName: 'Módulo 2', percentage: 75 },
-    { moduleName: 'Módulo 3', percentage: 90 },
-    { moduleName: 'Módulo 4', percentage: 65 },
-  ];
+  if (!context) {
+    return <div>Error: Context is not available.</div>;
+  }
+
+  const { state } = context;
+
+  const calculatePercentage = (moduleId: number): number => {
+    try {
+      const moduleQuestions = state.IsHero.filter(question => question.id === moduleId);
+      const totalQuestions = moduleQuestions.length;
+
+      if (totalQuestions === 0) {
+        return 100;
+      }
+
+      const totalPercentage = moduleQuestions.reduce((acc, question) => {
+        if (question.answer && typeof question.answer === 'string') {
+          const match = question.answer.match(/(\d+)%/);
+          const percentage = match ? parseInt(match[1], 10) : 0;
+          return acc + percentage;
+        } else {
+          return acc;
+        }
+      }, 0);
+
+      return totalPercentage / totalQuestions;
+    } catch (error) {
+      console.error('Error calculating percentage for module:', moduleId, error);
+      return 100;
+    }
+  };
+
+  const moduleData = state.modules.map((module) => ({
+    moduleName: module.module,
+    percentage: calculatePercentage(module.id),
+  }));
 
   const handleGoToDetails = () => {
-    navigate('/resumen-detalle')
-  }
+    navigate('/resumen-detalle');
+  };
 
   const handleGoToHome = () => {
-    navigate('/')
-  }
+    navigate('/');
+  };
 
   const handleGoToLuminometry = () => {
-    navigate('/luminometria')
-  }
+    navigate('/luminometria');
+  };
 
   const handleGoToETA = () => {
-    navigate('/seremi')
-  }
+    navigate('/seremi');
+  };
 
   return (
     <div className="summary-container">
@@ -36,18 +68,14 @@ const AuditSummary: React.FC = () => {
       <BPMGraph moduleData={moduleData} />
       <AverageModules />
       <div className="buttons-summary">
-      <button onClick={handleGoToDetails}>detalle</button>
-      <button onClick={handleGoToLuminometry}>Luminometria</button>
-      <button onClick={handleGoToETA}>ETA</button>
-      <button>KPI</button>
-      <button onClick={handleGoToHome}>Home</button>
+        <button onClick={handleGoToDetails}>detalle</button>
+        <button onClick={handleGoToLuminometry}>Luminometria</button>
+        <button onClick={handleGoToETA}>ETA</button>
+        <button>KPI</button>
+        <button onClick={handleGoToHome}>Home</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuditSummary
-
-
-
-
+export default AuditSummary;
