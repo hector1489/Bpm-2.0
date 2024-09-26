b307c9f491af48357e9d4b12d498fd272cf962a5
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-09-26 12:24:38 -0300

    incident summary

--- src/components/InciddentSummary/IncidentSummary.tsx
+++ src/components/InciddentSummary/IncidentSummary.tsx
@@ -5,7 +5,13 @@ const IncidentSummary: React.FC = () => {
   return (
     <div className="incident-summary-container">
       <p>IncidentSummary</p>
-      
+
+      <div className="gestion-incident">
+        
+      </div>
+      <div className="gestion-summary">
+
+      </div>
     </div>
   )
 }
