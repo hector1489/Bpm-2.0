b2c6cee2eaf18e5bf9be8cfd5eb489f4743de4f3
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-09-26 14:59:00 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: process.env.NODE_ENV === 'production' ? '/Bpm-2.0/' : '/'
+   base: process.env.VITE_BASE_URL || '/'
 })
  
