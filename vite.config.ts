36bedadfa350a02f4914789a70312f92eb341d5c
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-09-26 15:16:53 -0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: "https://hector1489.github.io/Bpm-2.0"
+   base: "/Bpm-2.0"
 })
  
