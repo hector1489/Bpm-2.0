60d968ba871901eab63ee63d35a30a4d312cbf14
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-09-30 14:37:28 -0300

    audit form

--- src/components/AuditForm/AuditForm.tsx
+++ src/components/AuditForm/AuditForm.tsx
@@ -1,26 +1,8 @@
-import { useState, useContext, useRef, useCallback } from 'react'
+import { useState, useContext, useRef } from 'react'
 import { AppContext } from '../../context/GlobalState'
 import './AuditForm.css'
 import { Answer } from '../../interfaces/interfaces'
 import { useNavigate } from 'react-router-dom'
-import { extractPercentage, getCurrentDate, calculateSolutionDate, getColorByPercentage } from '../../utils/utils';
-
-interface IAuditData {
-  numeroRequerimiento: number;
-  pregunta: string;
-  respuesta: string;
-  fecha: string;
-  auditor: string;
-  email: string;
-  nombreEstablecimiento: string;
-  responsableDelProblema: string;
-  solucionProgramada: string;
-  accionesCorrectivas: string;
-  estado: string;
-  photoUrl: string;
-}
-
-const DEFAULT_ANSWER = "Sin respuesta";
 
 const AuditForm: React.FC = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -30,7 +12,7 @@ const AuditForm: React.FC = () => {
   const navigate = useNavigate();
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const [auditData, setAuditData] = useState<IAuditData[]>([]);
+
 
   if (!context) {
     return <div>Error: El contexto no está disponible.</div>;
@@ -66,7 +48,6 @@ const AuditForm: React.FC = () => {
       setPhotoTaken(false);
     } else {
       addAnswers(updatedFormData);
-      collectAuditData();
       navigate('/resumen-auditoria');
     }
   };
@@ -123,40 +104,6 @@ const AuditForm: React.FC = () => {
       videoRef.current.srcObject = null;
     }
   }
-
-  // Función para recolectar todos los datos
-  const collectAuditData = useCallback(() => {
-    const email = state.auditSheetData.auditorEmail;
-    const auditor = state.userName || '';
-    const nombreEstablecimiento = state.auditSheetData.nombreEstablecimiento;
-    const responsableDelProblema = state.auditSheetData.supervisorEstablecimiento;
-    const photos = state.photos;
-
-    const newAuditData = state.IsHero
-      .filter((hero) => extractPercentage(hero.answer ?? DEFAULT_ANSWER) < 100)
-      .map((hero) => {
-        const criticidadColor = getColorByPercentage(extractPercentage(hero.answer ?? DEFAULT_ANSWER));
-        const solucionProgramada = calculateSolutionDate(criticidadColor);
-        const photo = photos.find(photo => photo.question === hero.question);
-
-        return {
-          numeroRequerimiento: hero.id,
-          pregunta: hero.question,
-          respuesta: hero.answer ?? DEFAULT_ANSWER,
-          fecha: getCurrentDate(),
-          auditor: auditor,
-          email: email,
-          nombreEstablecimiento: nombreEstablecimiento,
-          responsableDelProblema: responsableDelProblema,
-          solucionProgramada: solucionProgramada,
-          accionesCorrectivas: '',
-          estado: 'Abierto',
-          photoUrl: photo ? (photo.photoUrl || 'N/A') : 'N/A'
-        };
-      });
-
-    setAuditData(newAuditData);
-  }, [state]);
 
 
   return (
