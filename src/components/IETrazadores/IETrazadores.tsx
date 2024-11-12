7faa0d7f392daacae72f653439f29d84455b77be
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-11-12 11:35:00 -0300

    fix tra en IE

--- src/components/IETrazadores/IETrazadores.tsx
+++ src/components/IETrazadores/IETrazadores.tsx
@@ -2,7 +2,7 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import Highcharts3D from 'highcharts/highcharts-3d';
 import './IETrazadores.css';
-import { extractPrefix, extractPercentage } from '../../utils/utils'
+import { extractPrefix } from '../../utils/utils'
 
 Highcharts3D(Highcharts);
 
@@ -19,7 +19,7 @@ interface IETrazadoresProps {
 }
 
 const getColorByPercentageIETrazadores = (percentage: number) => {
-  
+
   if (percentage >= 75) {
     return '#2874a6';
   } else if (percentage >= 50) {
@@ -28,12 +28,11 @@ const getColorByPercentageIETrazadores = (percentage: number) => {
     return '#cb4335';
   }
 };
-
 const IETrazadores: React.FC<IETrazadoresProps> = ({ tablaDetails }) => {
 
   if (!tablaDetails || tablaDetails.length === 0) {
-  return <div>No hay datos disponibles para mostrar.</div>;
-}
+    return <div>No hay datos disponibles para mostrar.</div>;
+  }
 
   const categories = [
     'TRA CS 17',
@@ -49,20 +48,34 @@ const IETrazadores: React.FC<IETrazadoresProps> = ({ tablaDetails }) => {
     'TRA DOC 99'
   ];
 
-const filteredData = categories.map(category => {
+  const extractPercentageTRA = (answer: string): number => {
+    const match = answer.match(/(\d+)%/);
+    return match ? parseInt(match[1], 10) : 0;
+  };
+
+  const extractPercentageTRAVal = (answer: string): number | null => {
+    const match = answer.match(/(\d+)%/);
+    return match ? parseInt(match[1], 10) : null;
+  };
+  
+  const filteredData = categories.map(category => {
     const found = tablaDetails.find(detail => extractPrefix(detail.field3) === category);
-    const percentage = extractPercentage(found?.field4 || 'N/A');
+    const percentage = extractPercentageTRA(found?.field4 || 'N/A');
     const value = percentage;
+    const value2 = extractPercentageTRAVal(found?.field4 || 'N/A');
 
-    return { category, value, found };
-});
+    return { category, value, found, value2 };
+  });
 
-  if (filteredData.every(({ value }) => value === 0)) {
+  // Filtrar los valores válidos para el cálculo del promedio
+  const validData = filteredData.filter(({ value2 }) => value2 !== null);
+
+  if (validData.length === 0) {
     return <div>No hay datos disponibles para mostrar.</div>;
   }
 
-  const total = filteredData.reduce((sum, { value }) => sum + value, 0);
-  const average = filteredData.length > 0 ? (total / filteredData.length).toFixed(2) : 'N/A';
+  const total = validData.reduce((sum, { value2 }) => sum + (value2 || 0), 0);
+  const average = validData.length > 0 ? (total / validData.length).toFixed(2) : 'N/A';
 
   const dataWithColors = filteredData.map(({ value }) => ({
     y: value,
@@ -175,10 +188,10 @@ const filteredData = categories.map(category => {
       </div>
 
       <div className="cards-trazadores">
-        {filteredData.map(({  value, found }, index) => (
+        {filteredData.map(({ value2 , found }, index) => (
           <div key={index} className="card-trazadores">
             <h5>{found?.field3}</h5>
-            <p>Porcentaje: {value}%</p>
+            <p>Porcentaje: {value2 !== null ? `${value2}%` : 'N/A'}</p>
           </div>
         ))}
       </div>
@@ -188,6 +201,6 @@ const filteredData = categories.map(category => {
       </div>
     </div>
   );
-}
+};
 
 export default IETrazadores;
