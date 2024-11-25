0c2ee3c335e347aecc071057f7431dc235072b27
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-11-25 09:22:38 -0300

    fix chatbot none in mobile

--- src/components/Chatbot/Chatbot.tsx
+++ src/components/Chatbot/Chatbot.tsx
@@ -55,7 +55,7 @@ const Chatbot: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className='chatbot-component'>
       <div
         className={`chatbot-container ${isOpen ? 'open' : ''}`}
       >
